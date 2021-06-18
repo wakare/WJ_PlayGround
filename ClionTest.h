6ac4bfea059341534ecb29df8ec1e990da723571
wakare_no_kaze <wakare_no_kaze@outlook.com>
2021-06-18 23:30:57 +0800

    [Feature] First commit clion cpp project, this a nice ide

---
+++ ClionTest.h
@@ -0,0 +1,15 @@
+//
+// Created by jackjwang on 2021/6/18.
+//
+
+#ifndef WJ_PLAYGROUND_CLIONTEST_H
+#define WJ_PLAYGROUND_CLIONTEST_H
+
+#include <string.h>
+
+std::string MagicString()
+{
+    return "Clion Project!";
+}
+
+#endif //WJ_PLAYGROUND_CLIONTEST_H
